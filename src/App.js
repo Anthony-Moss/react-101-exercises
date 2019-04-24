b5155f1bd0041bd1eb4d98fdcbdcfe4401cab56c
Anthony Moss <asmoss93@gmail.com>
2019-04-24 00:32:32 -0400

    adds functions to change name to leetspeak and cipher

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Greet whom="Anthony"/>
+        <Greet whom="4NTHONY"/>
       </header>
     </div>
   );
--- src/Greet.js
+++ src/Greet.js
@@ -1,8 +1,63 @@
 import React from 'react';
+import { isNumber } from 'util';
 
 function lowerName(aName) {
     return aName.toLowerCase();
 }
+// function used to swap letter correctly in transl33t function
+let leetDictionary = {
+    A: 4,
+    E: 3,
+    G: 6,
+    I: 1,
+    O: 0,
+    S: 5,
+    T: 7,
+    }
+
+    // A first goes to a 4
+    // 4 then would need to go back to A
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value)
+}
+console.log(getKeyByValue(leetDictionary, 4))
+
+function transl33t(originalString) {
+    let result = '';
+    // console.log(originalString);
+    for (let i = 0; i < originalString.whom.length; i++) {
+        let theLetter = originalString.whom[i]
+        if (isNumber(theLetter) === true) {
+            let theKey = 'A' //getKeyByValue(leetDictionary, theLetter)
+            result += 'A'
+        } else if (leetDictionary[theLetter] !== undefined) {
+            result += leetDictionary[theLetter]
+        } else if (leetDictionary[theLetter] === undefined) {
+            result += theLetter
+        }
+        console.log(theLetter)
+    }
+    return result;
+}
+
+function cipher(originalName) {
+    let result = '';
+    let offset = 13;
+    for (let i = 0; i < originalName.whom.length; i++) {
+        let theLetter = originalName.whom[i];
+        let ascii = originalName.whom.charCodeAt(i);
+        if ((ascii) >=65 && (ascii) <= 90) {
+            theLetter = String.fromCharCode(((ascii - 65 + offset) % 26) + 65);  
+        } else if ((ascii) >=97 && (ascii) <= 122) {
+            theLetter = String.fromCharCode(((ascii - 97 + offset) % 26) + 97);    
+        }
+        result += theLetter;
+    }
+    return result;
+}
+
+
 
 class Greet extends React.Component {
     constructor(props) {
@@ -14,19 +69,43 @@ class Greet extends React.Component {
 
 
     render() {
-        return (
-            <h1>
-            Hello {lowerName(this.props.whom)}
-            </h1>
+        return (    
+            <div>
+                <h1>
+                    Hello {lowerName(this.props.whom)}
+                </h1>
+                <h2>
+                    Press the button and your name will change {this.state.whom}
+                </h2>
+                <button
+                    onClick={this._leetSpeak}
+                >L33T</button>
+                <button 
+                    onClick={this._cyphered}
+                >Cipher</button>
+            </div>
         )
     }
 
 
-    _lowerCase() {
+    _lowerCase = () => {
         this.setState({
             whom: (this.state).toLowerCase()
         });
     }
+
+    _leetSpeak = () => {
+        this.setState({
+            whom: transl33t(this.state)
+        });
+    }
+
+    _cyphered = () => {
+        this.setState({
+            whom: cipher(this.state)
+        });
+    }
+
 }
 
 
